abb45a20106fba0d0a4dc84415331f10e5e00750
Gabegar <gagarcia190@gmail.com>
2023-10-28 12:49:22 -0400

    updates minor styles on homepage shop now link

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
             <div className="relative flex flex-col items-center justify-center gap-3 text-4xl font-bold uppercase text-primary-purple">
                 <Link
                     to="store"
-                    className="absolute top-[calc(8rem+5dvh)] rounded-lg bg-primary-light-purple px-7 py-5 uppercase transition-all hover:scale-105 hover:bg-primary-purple hover:text-primary-light-purple md:top-[26dvh]"
+                    className="absolute top-[calc(6rem+5dvh)] rounded-lg bg-primary-light-purple px-7 py-5 text-center uppercase transition-all hover:scale-105 hover:bg-primary-purple hover:text-primary-light-purple md:top-[26dvh]"
                 >
                     Shop Now
                 </Link>
