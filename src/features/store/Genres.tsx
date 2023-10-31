348e760dfedbbe96bb777b18bdfe6c49f683732c
Gabegar <gagarcia190@gmail.com>
2023-10-31 19:25:03 -0400

    updates styles on Genres component

--- src/features/store/Genres.tsx
+++ src/features/store/Genres.tsx
@@ -26,14 +26,14 @@ const Genres = () => {
     };
 
     return (
-        <aside className="flex max-h-[35rem] gap-2 overflow-x-scroll scroll-smooth pr-6 lg:sticky lg:top-4 lg:h-full lg:flex-col lg:overflow-y-scroll">
+        <aside className="flex max-h-[35rem] gap-2 overflow-x-auto scroll-smooth pr-6 lg:sticky lg:top-4 lg:h-full lg:flex-col lg:overflow-y-scroll">
             <h2 className="hidden text-center text-2xl font-semibold text-primary-purple lg:block">
                 Genres
             </h2>
             {sortedGenresData.map((genre) => (
                 <button
                     key={genre.id}
-                    className="min-w-max rounded-lg bg-primary-purple px-2 py-1 font-bold text-slate-50"
+                    className="rounded-lg bg-primary-purple px-2 py-1 font-bold text-slate-50"
                     onClick={() => {
                         handleGenreClick(genre.genre);
                     }}
