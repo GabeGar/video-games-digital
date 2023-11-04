888c39e52087d3bade3f2705cf759866f7fd6686
Gabegar <gagarcia190@gmail.com>
2023-11-03 23:15:14 -0400

    adds focus to input on opening searchMenu

--- src/features/overlay/SearchMenuOverlay/SearchMenuFormButton.tsx
+++ src/features/overlay/SearchMenuOverlay/SearchMenuFormButton.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 import { useAppDispatch, useAppSelector } from '../../../hooks/app-hooks';
 import {
@@ -15,6 +15,7 @@ import SearchMenuGames from './SearchMenuGames';
 const BASE_INPUT_STYLES = `h-12 rounded-lg bg-slate-100 text-center text-[calc(.75rem+1dvw)] sm:text-xl font-semibold tracking-wide text-primary-purple outline-none transition-all duration-150 placeholder:px-10 placeholder:text-primary-purple/70 focus:bg-slate-50 focus:ring-2 focus:ring-primary-purple group-hover:outline-2 group-hover:outline-offset-0 group-hover:outline-primary-purple group-hover:placeholder:text-primary-purple/70 sm:block lg:w-[35rem] xl:w-[50rem]`;
 
 const SearchMenuFormButton = () => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
     const dispatch = useAppDispatch();
     const { isSearchMenuOpen, searchQuery } = useAppSelector(
         (state) => state.searchMenuOverlay,
@@ -23,6 +24,11 @@ const SearchMenuFormButton = () => {
     useEffect(() => {
         // Prevent accidental firing of the fetchGamesByQuery function on mount. SearchMenu MUST have isSearchMenuOpen state be true, in order for the intended effect to trigger.
         if (!isSearchMenuOpen) return;
+
+        // Focus on the input field when the form is rendered
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
 
         // Prevent premature firing of fetch query requests and clear any game results, fetched or otherwise.
         if (searchQuery.length <= 2) {
@@ -72,6 +78,7 @@ const SearchMenuFormButton = () => {
                         <SearchIcon />
                     </button>
                     <input
+                        ref={inputRef}
                         className={`${BASE_INPUT_STYLES} md:w-[30rem]`}
                         type="text"
                         name="Search"
