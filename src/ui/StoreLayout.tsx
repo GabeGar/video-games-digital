b83c6189903906884ff5644a14a2796d6fdbea4e
Gabegar <gagarcia190@gmail.com>
2023-11-04 22:12:58 -0400

    updates naming of imported store components

--- src/ui/StoreLayout.tsx
+++ src/ui/StoreLayout.tsx
@@ -1,11 +1,11 @@
-import Games from '../features/store/StoreGames';
-import GameGenres from '../features/store/StoreGameGenres';
+import StoreGames from '../features/store/StoreGames';
+import StoreGameGenres from '../features/store/StoreGameGenres';
 
 const StoreLayout = () => {
     return (
         <div className="grid p-3 lg:grid-cols-storeLayoutGrid lg:px-9">
-            <GameGenres />
-            <Games />
+            <StoreGameGenres />
+            <StoreGames />
         </div>
     );
 };
